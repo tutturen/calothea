21c630b4246459b1afb9d9a60aa47611c89a16fb
Sjur Waagbø <sjur.waagbo@gmail.com>
2015-02-24 10:48:14 +0100

    Added getters and constructor.

--- src/models/Rom.java
+++ src/models/Rom.java
@@ -4,4 +4,24 @@ public class Rom {
 	String romNavn;
 	int romNr;
 	int maxAntall;
+	
+	public Rom(String romNavn, int romNr, int maxAntall) {
+		this.romNavn = romNavn;
+		this.romNr = romNr;
+		this.maxAntall = maxAntall;
+	}
+	
+	public int getAntall() {
+		return maxAntall;
+	}
+	
+	public String getRomnavn() {
+		return romNavn;
+	}
+	
+	public int getRomNr() {
+		return romNr;
+	}
+	
+	
 }
