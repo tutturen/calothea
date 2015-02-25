9025dd46f9a3d0749d4fbdabaa6ddb5e36985700
simehe <simhellem@gmail.com>
2015-02-25 11:06:45 +0100

    Begynt på kalenderklasen

--- src/models/Kalender.java
+++ src/models/Kalender.java
@@ -1,5 +1,28 @@
 package models;
 
+import java.util.ArrayList;
+
 public class Kalender {
+	
+	ArrayList<Avtale> avtaleListe;
+	String navn;
+	int kalenderID;
+	
+	
+	//Lager ikke konstruktor fordi objektet vil komme fra server
+	
+	//I alle disse metodene maa vi oppdatere server 
+	public void setNavn(String navn){
+		this.navn = navn;
+	}
+	
+	public void setID(int ID){
+		this.kalenderID = ID;
+	}
+	
+	public void setAvtaler(ArrayList<Avtale> avtaleliste){
+		this.avtaleListe = avtaleliste;
+	}
+	
 
 }
