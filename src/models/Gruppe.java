2ba23b91dc9bf5bf7789794c01f899455af4597e
Per Oskar Isdahl <peroi@stud.ntnu.no>
2015-02-27 18:28:27 +0100

    Skrev ferdig Gruppe-klassen. Ikke testet enda.

--- src/models/Gruppe.java
+++ src/models/Gruppe.java
@@ -3,58 +3,90 @@ package models;
 import java.util.ArrayList;
 
 public class Gruppe {
-	
+
 	Kalender gruppeKalender;
 	ArrayList<User> gruppeMedlemmer;
-	ArrayList<Gruppe> subgrupper;
+	ArrayList<Gruppe> subGrupper; // ikke i klassediagrammet i exercise 5
 	String gruppeNavn;
-	
+
 	public Gruppe(Kalender gruppeKalender, String gruppeNavn) {
 		gruppeMedlemmer = new ArrayList<User>();
-		subgrupper = new ArrayList<Gruppe>();
+		subGrupper = new ArrayList<Gruppe>();
 		this.gruppeKalender = gruppeKalender;
+		if (!isValidName(gruppeNavn)) {
+			throw new IllegalArgumentException("Illegal gruppeNavn");
+		}
 		this.gruppeNavn = gruppeNavn;
-		
+
 	}
-	
+
+	private boolean isValidName(String name) {
+		return (name != null && !name.equals(""));
+	}
+
 	public ArrayList<User> getGruppeMedlemmer() {
 		return gruppeMedlemmer;
 	}
-	
+
 	public ArrayList<Gruppe> getSubgrupper() {
-		return subgrupper;
+		return subGrupper;
 	}
 
-	public Kalender getGruppeKalender() {
+	public Kalender getGruppeKalender() { 
 		return gruppeKalender;
 	}
-	
-	public String getGruppeNavn() {
+
+	public String getGruppeNavn() { 
 		return gruppeNavn;
 	}
-	
+
 	public Kalender getKalender() {
-		
-	}
-	
-	public ArrayList getMedlemmer() {
-		
-	}
-	
-	public void addMedlem(User nyttMedlem) {
-		
-	}
-	
-	public void removeMedlem(User fjerneMedlem) {
-		
-	}
-	
-	public void addKalender(int kalenderID) {
-		
-	}
-	
-	public void removeKalender(int kalenderID) {
-		
+		return gruppeKalender;
+
 	}
 
+	public ArrayList<User> getMedlemmer() {
+		return gruppeMedlemmer;
+
+	}
+
+	public void addMedlem(User user) {
+		if (gruppeMedlemmer.contains(user)) {
+			throw new IllegalStateException("User already in group");
+		}
+		gruppeMedlemmer.add(user);
+	}
+
+	public void removeMedlem(User user) {
+		if (!gruppeMedlemmer.contains(user)) {
+			throw new IllegalStateException("User not in group");
+		}
+		gruppeMedlemmer.remove(user);
+	}
+
+	public void setKalender(Kalender kalender) { // byttet ut addKalender og removeKalender med en setKalender da du uansett bare kan ha en
+		gruppeKalender = kalender;
+	}
+
+	public void setGruppeNavn(String gruppeNavn) { 
+		if (!isValidName(gruppeNavn)) {
+			throw new IllegalArgumentException("Illegal gruppeNavn");
+		}
+		this.gruppeNavn = gruppeNavn;
+	}
+
+	public void addSubGruppe(Gruppe gruppe) { // metode ikke i klassediagram
+		if (subGrupper.contains(gruppe)) {
+			throw new IllegalStateException("Group already a subgroup");
+		}
+		subGrupper.add(gruppe);
+	}
+
+	public void removeSubGruppe(Gruppe gruppe) { // metode ikke i klassediagram
+		if (!subGrupper.contains(gruppe)) {
+			throw new IllegalStateException("Group not a subgroup");
+		}
+		subGrupper.remove(gruppe);
+	}
+	
 }
