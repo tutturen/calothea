ecd9388ac43ade479da8de8dddd2561bdd358c49
Thor Even Tutturen <t.tutturen@gmail.com>
2015-03-04 10:16:44 +0100

    Changes to aktivitet.

--- src/tests/AktivitetTest.java
+++ src/tests/AktivitetTest.java
@@ -20,79 +20,84 @@ public class AktivitetTest {
 
 	Date startDate = new Date(System.currentTimeMillis() + 5000L);
 	Date endDate = new Date(System.currentTimeMillis() + 10000L);
+	String navn = "Superkalender";
 	Rom rom = new Rom("Rom 401", 401, 13);
 	Rom smallRom = new Rom("Lite rom", 25, 2);
 	
 	@Test
 	public void testConstructor() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
+		Aktivitet aktivitet = new Aktivitet(owner, navn,  startDate, endDate);
 		assertEquals(aktivitet.getBrukereInvitert().size(), 0);
 		assertEquals(aktivitet.getDeltagere().size(), 1);
 		assertEquals(aktivitet.getEier(), owner);
 		assertEquals(aktivitet.getEndDate(), endDate);
 		assertEquals(aktivitet.getStartDate(), startDate);
-		assertEquals(aktivitet.getRom(), rom);
+		assertEquals(aktivitet.getNavn(), navn);
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testCantInviteOwner() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
 		aktivitet.addToInvitedList(owner);
 	}
 	
 	@Test
 	public void testSendInvite() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
-		aktivitet.addToInvitedList(otherUser1);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
+		aktivitet.setRom(rom);
+		aktivitet.addToInvitedList(otherUser1, false);
 		assertEquals(aktivitet.getBrukereInvitert().size(), 1);
-		aktivitet.addToInvitedList(otherUser2);
+		aktivitet.addToInvitedList(otherUser2, false);
 		assertEquals(aktivitet.getBrukereInvitert().size(), 2);
 		assertEquals(aktivitet.getBrukereInvitert().contains(otherUser1), true);
 	}
 	
 	@Test(expected = IllegalArgumentException.class)
 	public void testEndDateBeforeStartDate() {
-		new Aktivitet(owner, endDate, startDate, rom);
+		new Aktivitet(owner, navn, endDate, startDate);
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testInviteTooMany() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, smallRom);
-		aktivitet.addToInvitedList(otherUser1);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
+		aktivitet.setRom(smallRom);
+		aktivitet.addToInvitedList(otherUser1, false);
 		assertTrue(aktivitet.getBrukereInvitert().contains(otherUser1));
-		aktivitet.addToInvitedList(otherUser1);
+		aktivitet.addToInvitedList(otherUser1, false);
 	}
 	
 	@Test
 	public void testAcceptInvite() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
-		aktivitet.addToInvitedList(otherUser1);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
+		aktivitet.setRom(rom);
+		aktivitet.addToInvitedList(otherUser1, false);
 		assertEquals(aktivitet.getBrukereInvitert().size(), 1);
 		assertEquals(aktivitet.getDeltagere().size(), 1);
-		aktivitet.acceptInvitation(otherUser1);
+		aktivitet.acceptInvitation(otherUser1, false);
 		assertEquals(aktivitet.getBrukereInvitert().size(), 0);
 		assertEquals(aktivitet.getDeltagere().size(), 2);
 	}
 	
 	@Test
 	public void testRemoveInvite() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
-		aktivitet.addToInvitedList(otherUser1);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
+		aktivitet.setRom(rom);
+		aktivitet.addToInvitedList(otherUser1, false);
 		assertEquals(aktivitet.getBrukereInvitert().contains(otherUser1), true);
 		
-		aktivitet.removeFromInvitedList(otherUser1);
+		aktivitet.removeFromInvitedList(otherUser1, false);
 		assertEquals(aktivitet.getBrukereInvitert().contains(otherUser1), false);
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testRemoveOwnerInvite() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
 		aktivitet.removeFromInvitedList(owner);
 	}
 	
 	@Test(expected = IllegalStateException.class)
 	public void testRemoveNonExistentInvite() {
-		Aktivitet aktivitet = new Aktivitet(owner, startDate, endDate, rom);
+		Aktivitet aktivitet = new Aktivitet(owner, navn, startDate, endDate);
 		aktivitet.removeFromInvitedList(otherUser1);
 	}
 	
